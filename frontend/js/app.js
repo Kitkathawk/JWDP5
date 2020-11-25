ea728ed16991da6ece6a0e88f55a9df08cd8253e
Kitkathawk <susana.vale.lopes@gmail.com>
2020-11-25 23:50:01 +0000

    commit for bill

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,12 +1,14 @@
 // PRODUCT LISTING
 
-fetch('http://localhost:3002/api/cameras')
+/*fetch('http://localhost:3002/api/cameras')
   .then(response => response.json())
   .then(function(data){
   	
   	let catalog = document.getElementById('listing');
   	
   	for (let i = 0; i < data.length; i++) {
+
+  		// DOM ELEMENTS
 
   		let li = document.createElement('li');
   		let article = document.createElement('article');
@@ -15,12 +17,7 @@ fetch('http://localhost:3002/api/cameras')
   		let itemName = document.createElement('h5');
   		let itemPrice = document.createElement('p');
 
-  		itemImage.setAttribute('src', data[i].imageUrl);
-  		itemName.textContent = data[i].name;
-  		itemPrice.textContent = data[i].price;
-
-  		li.classList.add('col', 'col-lg-4', 'card');
-  		itemImage.classList.add('card-img-top', 'rounded', 'shadow');
+  		// HTML STRUCTURE
 
   		link.append(itemImage);
   		article.append(link);
@@ -29,18 +26,71 @@ fetch('http://localhost:3002/api/cameras')
   		li.append(article);
   		catalog.append(li);
 
-  		link.setAttribute('href', 'http://localhost:3002/api/cameras/' + data[i]._id);
+  		// VALUES & ATTRIBUTES
+
+  		link.setAttribute('href', data[i]._id);
+  		itemImage.setAttribute('src', data[i].imageUrl);
+  		itemName.textContent = data[i].name;
+  		itemPrice.textContent = data[i].price;
+
+  		// CLASSES 
+
+  		li.classList.add('col-12', 'col-md-6', 'col-lg-4', 'card');
+  		itemImage.classList.add('card-img-top', 'rounded', 'shadow');
   	}
 
-  });
+  });*/
 
+  let _id = '5be1ed3f1c9d44000030b061';
 
-// PRODUCT DETAIL
-
-fetch('http://localhost:3002/api/cameras/_id')
+  fetch(`http://localhost:3002/api/cameras/${_id}`)
   .then(response => response.json())
   .then(function(data){
 
   	console.log(data);
 
+
   });
+
+
+// PRODUCT DETAIL //5be1ed3f1c9d44000030b061
+
+/*fetch('http://localhost:3002/api/cameras')
+  .then(response => response.json())
+  .then(function(data){
+
+	let productDetail = document.getElementById('product-detail');
+
+	for (let i = 0; i < data.length; i++) {	
+
+		// DOM ELEMENTS
+
+  		let article = document.createElement('article');
+  		let itemImage = document.createElement('img');
+  		let itemName = document.createElement('h5');
+  		let itemPrice = document.createElement('p');
+  		let itemDescription = document.createElement('p');
+  		let chooseLens = document.createElement('select');
+  		let lensOption = document.createElement('option');
+
+  		// HTML STRUCTURE
+
+  		chooseLens.append(lensOption);
+  		article.append(itemImage);
+  		article.append(itemName);
+  		article.append(itemPrice);
+  		article.append(itemDescription);
+  		article.append(chooseLens);
+  		productDetail.append(article);
+
+  		// VALUES & ATTRIBUTES
+
+  		itemImage.setAttribute('src', data[i].imageUrl);
+  		itemName.textContent = data[i].name;
+  		itemPrice.textContent = data[i].price;
+  		itemDescription.textContent = data[i].description;
+  	
+  	}
+
+});*/
+
